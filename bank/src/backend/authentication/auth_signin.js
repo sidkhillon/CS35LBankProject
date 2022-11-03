4d30a4baeff54dbbfcfdab1c18b41f5edcd2025c
Jackson Steele <jacksonrobertsteele@gmail.com>
2022-11-03 16:15:15 -0700

    Basic authentication functions

---
+++ bank/src/backend/authentication/auth_signin.js
@@ -0,0 +1,11 @@
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
---
+++ bank/src/backend/authentication/auth_signup.js
@@ -0,0 +1,12 @@
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
