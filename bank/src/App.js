5fad4bfe91835bb8c66653d21c8359f42424d522
Juskeerat Anand <juskeerat@gmail.com>
2022-11-11 16:08:23 -0800

    add send money screen

--- bank/src/App.js
+++ bank/src/App.js
@@ -6,6 +6,7 @@ import Login from "./Login"
 import LoginForm from "./LoginForm"
 import Main from "./Main"
 import SignupForm from './SignupForm'
+import Pay from './Pay'
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/loginform" element={<LoginForm/>} />
         <Route path="/signup" element={<SignupForm />} />
+        <Route path="/pay" element={<Pay/>} />
         <Route path="" element={<Main />} />
+        
       </Routes>
     </BrowserRouter>
   )
