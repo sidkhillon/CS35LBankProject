7d6f201fc001a69f184622b9734b2ac6feba0721
jcarrot223 <juskeerat@gmail.com>
2022-11-17 21:59:17 -0800

    logo

--- bank/src/Navbar.js
+++ bank/src/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import crownlogo from './logo.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function NavbarComponent() {
     const user = "TempUser Name"
+    // var icon = (
+    //     <span class = 'logo'>
+    //         <a href = '/'/> 
+    //         <img src = './login.png' height = "33" width = "120" /> 
+    //     </span>
+    // );
     return (
         <div>
             <Navbar bg="light">
                 <Container>
-                    <Navbar.Brand>Opes</Navbar.Brand>
+                    <Navbar.Brand>
+                        <img src = {crownlogo} height = "60"/> 
+                           Opes
+                    </Navbar.Brand>
                     <Navbar.Text>{user}</Navbar.Text>
                 </Container>
             </Navbar>
